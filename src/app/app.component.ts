0113f9bab6bd104373623cdf81578a497a405f97
Brenda Franco and Susan Southall <susan@>
2018-05-03 10:19:10 -0700

    Add and style list of kegs.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Keg } from './models/keg.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'CamelCase TapRoom';
+
+  masterKegList: Keg[] = [
+    new Keg('Nectarine Premiere', 'de Garde Brewing', '7.10%'),
+    new Keg('Notorious Triple IPA', 'Boneyard Beer Company', '12.00%'),
+    new Keg('5	Double Stack', 'Great Notion Brewing', '10.00%')
+  ]
 }
